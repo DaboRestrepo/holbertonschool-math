5fe664b456378aa09505d1d66b2c39a06b35f2c8
DaboRestrepo <daborestrepo@gmail.com>
2021-11-12 14:07:07 -0500

    Second intent

--- 0-display.c
+++ 0-display.c
@@ -9,12 +9,12 @@ void display_complex_number(complex c)
 {
 	if (c.im == 0)
 	{
-		printf("%f\n", c.re);
+		printf("%.0f\n", c.re);
 	}
 	if (c.im > 0)
 	{
-		printf("%f + %fi\n", c.re, c.im);
+		printf("%.0f + %.0fi\n", c.re, c.im);
 	}
 	else if (c.im < 0)
-	printf("%f - %fi\n", c.re, -c.im);
+	printf("%.0f - %.0fi\n", c.re, -c.im);
 }
